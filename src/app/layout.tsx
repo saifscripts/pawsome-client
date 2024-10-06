71acdc19bc4becb2d78cec03f592485b24369396
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-10-06 14:32:52 +0600

    next ui setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,66 @@
-import type { Metadata } from 'next';
-import './globals.css';
+import '@/styles/globals.css';
+import { Link } from '@nextui-org/link';
+import clsx from 'clsx';
+import { Metadata, Viewport } from 'next';
+
+import { Providers } from './providers';
+
+import { Navbar } from '@/components/navbar';
+import { fontSans } from '@/config/fonts';
+import { siteConfig } from '@/config/site';
 
 export const metadata: Metadata = {
-  title: 'Pawsome | Pet Care Tips & Heartwarming Stories',
-  description:
-    'Dive into expert pet care tips on grooming, nutrition, and health. Explore inspiring stories of rescue, adoption, and the unbreakable bond between pets and their owners. Pawsome Insights offers practical advice and heartwarming tales for every pet lover!',
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+          <div className="relative flex flex-col h-screen">
+            <Navbar />
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              {children}
+            </main>
+            <footer className="w-full flex items-center justify-center py-3">
+              <Link
+                isExternal
+                className="flex items-center gap-1 text-current"
+                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+                title="nextui.org homepage"
+              >
+                <span className="text-default-600">Powered by</span>
+                <p className="text-primary">NextUI</p>
+              </Link>
+            </footer>
+          </div>
+        </Providers>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main>Welcome to pawsome!</main>;
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      Hello Pawsome People
+    </section>
+  );
 }
