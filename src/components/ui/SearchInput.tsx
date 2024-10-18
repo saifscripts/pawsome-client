1973567446607650d124b4460ae64dc4c39e6028
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-10-18 23:04:54 +0600

    implement open search modal on ctrl + k press

--- src/components/ui/SearchInput.tsx
+++ src/components/ui/SearchInput.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from '@nextui-org/button';
+import { Input } from '@nextui-org/input';
 import { Kbd } from '@nextui-org/kbd';
 import {
   Modal,
@@ -11,9 +12,25 @@ import {
   useDisclosure,
 } from '@nextui-org/modal';
 import { SearchIcon } from 'lucide-react';
+import { useEffect } from 'react';
 
 export default function SearchInput() {
   const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.key === 'k') {
+        e.preventDefault();
+        onOpen();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   return (
     <>
@@ -40,7 +57,26 @@ export default function SearchInput() {
         onOpenChange={onOpenChange}
       >
         <ModalContent>
-          <ModalHeader className="block w-full pb-2">Search</ModalHeader>
+          <ModalHeader className="block w-full pb-2">
+            <Input
+              aria-label="Search"
+              classNames={{
+                inputWrapper: 'bg-default-100 lg:max-w-48',
+                input: 'text-sm',
+              }}
+              endContent={
+                <Kbd className="hidden lg:inline-block" keys={['command']}>
+                  K
+                </Kbd>
+              }
+              labelPlacement="outside"
+              placeholder="Search..."
+              startContent={
+                <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+              }
+              type="search"
+            />
+          </ModalHeader>
           <ModalBody>Hello</ModalBody>
           <ModalFooter className="block w-full"></ModalFooter>
         </ModalContent>
