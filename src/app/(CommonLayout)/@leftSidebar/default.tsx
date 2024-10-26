3b6c063efcb833ffc2c04d73882d4cb8bc9173c4
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-10-26 10:44:58 +0600

    add sidebar menu for feeds

--- src/app/(CommonLayout)/@leftSidebar/default.tsx
+++ src/app/(CommonLayout)/@leftSidebar/default.tsx
@@ -1,8 +1,12 @@
 import SubscriptionCard from '@/components/subscription/SubscriptionCard';
+import { Divider } from '@nextui-org/divider';
+import SidebarMenu from './_components/SidebarMenu';
 
 export default function DefaultLeftSidebar() {
   return (
     <aside className="w-[300px] h-full overflow-y-auto border-r border-divider p-4">
+      <SidebarMenu />
+      <Divider className="my-4" />
       <SubscriptionCard />
     </aside>
   );
